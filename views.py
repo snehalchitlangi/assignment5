98f0dc9dca23031808f43a17950d10fd350c49f5
snehalchitlangi <53406245+snehalchitlangi@users.noreply.github.com>
2020-07-27 06:38:15 +0530

    Create views.py

---
+++ views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+
+from .forms import SignUpForm
+
+
+def home(request):
+    return render(request, 'login/home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'login/signup.html', { 'form' : form })
